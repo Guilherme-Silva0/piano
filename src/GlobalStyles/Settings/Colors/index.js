afb5f0bad8eb2d9883f3596a438e9b1f8c056183
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-26 21:27:23 -0300

    adding color variables

---
+++ src/GlobalStyles/Settings/Colors/index.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const Colors = createGlobalStyle`
+    :root{
+        --color-zero: #efefef;
+        --color-negative: #111;
+        --color-fist: #1f2937;
+        --color-second: #111827;
+    }
+`;
+
+export default Colors;
