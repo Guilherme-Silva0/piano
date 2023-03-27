bde90e5b1a7cde71e23be6aa0d8907480dcabc5e
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-27 20:22:16 -0300

    centering elements

--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -6,6 +6,9 @@ const Base = createGlobalStyle`
         font-family: 'Outfit', sans-serif;
         height: 100vh;
         width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 `;
 
