c73ea84398886f3ab7291e46c27b1dd07c94381a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-28 20:56:26 -0300

    optimizing play note

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
+import React, { useState, useEffect } from "react";
 import Piano from "./components/Piano";
 
+const musicalNotes = {
+  Do: { frequency: 261.625519 },
+  "Do#": { frequency: 277.182648 },
+  Re: { frequency: 293.664734 },
+  "Re#": { frequency: 311.126984 },
+  Mi: { frequency: 329.627533 },
+  Fa: { frequency: 349.228241 },
+  "Fa#": { frequency: 369.994385 },
+  Sol: { frequency: 391.995392 },
+  "Sol#": { frequency: 415.304688 },
+  La: { frequency: 440.0 },
+  "La#": { frequency: 466.163788 },
+  Si: { frequency: 493.883301 },
+};
+
 const App = () => {
-  const musicalNotes = {
-    Do: { frequency: 261.625519 },
-    "Do#": { frequency: 277.182648 },
-    Re: { frequency: 293.664734 },
-    "Re#": { frequency: 311.126984 },
-    Mi: { frequency: 329.627533 },
-    Fa: { frequency: 349.228241 },
-    "Fa#": { frequency: 369.994385 },
-    Sol: { frequency: 391.995392 },
-    "Sol#": { frequency: 415.304688 },
-    La: { frequency: 440.0 },
-    "La#": { frequency: 466.163788 },
-    Si: { frequency: 493.883301 },
-  };
+  const [audioContext, setAudioContext] = useState(null);
+
+  useEffect(() => {
+    const context = new AudioContext();
+    setAudioContext(context);
+
+    return () => {
+      context.close();
+    };
+  }, []);
 
   const playNote = (note) => {
-    let context = new AudioContext();
-    let oscillator = context.createOscillator();
+    if (!audioContext) return;
 
+    const oscillator = audioContext.createOscillator();
     oscillator.frequency.value = musicalNotes[note].frequency;
 
-    let volume = context.createGain();
-    volume.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 4);
+    const volume = audioContext.createGain();
+    volume.gain.exponentialRampToValueAtTime(
+      0.00001,
+      audioContext.currentTime + 4
+    );
 
     oscillator.connect(volume);
-    volume.connect(context.destination);
+    volume.connect(audioContext.destination);
 
     oscillator.type = "triangle";
     oscillator.start();
+
+    setTimeout(() => {
+      oscillator.stop();
+    }, 4000);
   };
 
   return (
     <>
-      <Piano />
+      <Piano playNote={playNote} />
     </>
   );
 };
