ad7a9bc2166e1d1d92d0f7e8eca00f38abbc2134
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-28 18:45:58 -0300

    adding frequencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import Piano from "./components/Piano";
 
 const App = () => {
+  const musicalNotes = {
+    Do: { frequency: 261.625519 },
+    "Do#": { frequency: 277.182648 },
+    Re: { frequency: 293.664734 },
+    "Re#": { frequency: 311.126984 },
+    Mi: { frequency: 329.627533 },
+    Fa: { frequency: 349.228241 },
+    "Fa#": { frequency: 369.994385 },
+    Sol: { frequency: 391.995392 },
+    "Sol#": { frequency: 415.304688 },
+    La: { frequency: 440.0 },
+    "La#": { frequency: 466.163788 },
+    Si: { frequency: 493.883301 },
+  };
+
   return (
     <>
       <Piano />
