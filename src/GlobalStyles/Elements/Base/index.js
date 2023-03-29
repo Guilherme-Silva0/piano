59996d7c2bec40a255d92884c5e59253fca850c5
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-29 15:34:01 -0300

    paragraph styling

--- src/GlobalStyles/Elements/Base/index.js
+++ src/GlobalStyles/Elements/Base/index.js
@@ -9,7 +9,21 @@ const Base = createGlobalStyle`
         display: flex;
         align-items: center;
         justify-content: center;
+        flex-direction: column;
+        
+        
+        & p {
+            width: 100%;
+            color: var(--color-zero);
+            text-align: center;
+            text-transform: uppercase;
+            font-size: 4vw;
+            color: var(--color-third);
+            padding: 20px 0;
+            filter: drop-shadow(0 0 10px var(--color-third));
+        }
     }
+
 `;
 
 export default Base;
