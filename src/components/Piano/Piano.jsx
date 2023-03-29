658e66ee6d14468a22673fe3ab71017d208b4893
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-28 21:13:14 -0300

    assigning playNote function to each note

--- src/components/Piano/Piano.jsx
+++ src/components/Piano/Piano.jsx
@@ -2,20 +2,23 @@ import PianoStyles from "./Piano.styles";
 import Key from "./../Key";
 import SharpKey from "./../SharpKey";
 
-const Piano = () => (
+const Piano = ({ playNote }) => (
   <PianoStyles>
-    <Key dataNote="Do" />
-    <SharpKey dataNote="Do#" />
-    <Key dataNote="Re" />
-    <SharpKey dataNote="Re#" />
-    <Key dataNote="Mi" />
-    <Key dataNote="Fa" />
-    <SharpKey dataNote="Fa#" />
-    <Key dataNote="Sol" />
-    <SharpKey dataNote="Sol#" />
-    <Key dataNote="la" />
-    <SharpKey dataNote="la#" />
-    <Key dataNote="Si" />
+    <Key dataNote="Do" onClick={(e) => playNote(e.target.dataset.note)} />
+    <SharpKey dataNote="Do#" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Re" onClick={(e) => playNote(e.target.dataset.note)} />
+    <SharpKey dataNote="Re#" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Mi" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Fa" onClick={(e) => playNote(e.target.dataset.note)} />
+    <SharpKey dataNote="Fa#" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Sol" onClick={(e) => playNote(e.target.dataset.note)} />
+    <SharpKey
+      dataNote="Sol#"
+      onClick={(e) => playNote(e.target.dataset.note)}
+    />
+    <Key dataNote="La" onClick={(e) => playNote(e.target.dataset.note)} />
+    <SharpKey dataNote="La#" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Si" onClick={(e) => playNote(e.target.dataset.note)} />
   </PianoStyles>
 );
 
