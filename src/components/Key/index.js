3a524f4bf079d6ed49ff73a23ba64ef4bee6260b
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-29 15:30:49 -0300

    styling component text

--- src/components/Key/index.js
+++ src/components/Key/index.js
@@ -4,10 +4,16 @@ const Key = styled.button.attrs((props) => ({ "data-note": props.dataNote }))`
   background-color: var(--color-zero);
   border: 1px solid var(--color-negative);
   border-radius: 0 0 15px 15px;
-  padding: 0;
   width: 100%;
   cursor: pointer;
   transition: 0.2s;
+  color: var(--color-negative);
+  font-size: 2.5vw;
+  font-weight: bold;
+  display: flex;
+  align-items: end;
+  justify-content: center;
+  padding-bottom: 20px;
 
   &:active {
     opacity: 0.8;
