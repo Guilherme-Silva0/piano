2584f8bf6d8ce28eb322330fde749f3a871c6e03
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-29 15:32:42 -0300

    adding text to component

--- src/components/Piano/Piano.jsx
+++ src/components/Piano/Piano.jsx
@@ -4,21 +4,42 @@ import SharpKey from "./../SharpKey";
 
 const Piano = ({ playNote }) => (
   <PianoStyles>
-    <Key dataNote="Do" onClick={(e) => playNote(e.target.dataset.note)} />
-    <SharpKey dataNote="Do#" onClick={(e) => playNote(e.target.dataset.note)} />
-    <Key dataNote="Re" onClick={(e) => playNote(e.target.dataset.note)} />
-    <SharpKey dataNote="Re#" onClick={(e) => playNote(e.target.dataset.note)} />
-    <Key dataNote="Mi" onClick={(e) => playNote(e.target.dataset.note)} />
-    <Key dataNote="Fa" onClick={(e) => playNote(e.target.dataset.note)} />
-    <SharpKey dataNote="Fa#" onClick={(e) => playNote(e.target.dataset.note)} />
-    <Key dataNote="Sol" onClick={(e) => playNote(e.target.dataset.note)} />
-    <SharpKey
-      dataNote="Sol#"
-      onClick={(e) => playNote(e.target.dataset.note)}
-    />
-    <Key dataNote="La" onClick={(e) => playNote(e.target.dataset.note)} />
-    <SharpKey dataNote="La#" onClick={(e) => playNote(e.target.dataset.note)} />
-    <Key dataNote="Si" onClick={(e) => playNote(e.target.dataset.note)} />
+    <Key dataNote="Do" onClick={(e) => playNote(e.target.dataset.note)}>
+      A
+    </Key>
+    <SharpKey dataNote="Do#" onClick={(e) => playNote(e.target.dataset.note)}>
+      W
+    </SharpKey>
+    <Key dataNote="Re" onClick={(e) => playNote(e.target.dataset.note)}>
+      S
+    </Key>
+    <SharpKey dataNote="Re#" onClick={(e) => playNote(e.target.dataset.note)}>
+      E
+    </SharpKey>
+    <Key dataNote="Mi" onClick={(e) => playNote(e.target.dataset.note)}>
+      D
+    </Key>
+    <Key dataNote="Fa" onClick={(e) => playNote(e.target.dataset.note)}>
+      F
+    </Key>
+    <SharpKey dataNote="Fa#" onClick={(e) => playNote(e.target.dataset.note)}>
+      T
+    </SharpKey>
+    <Key dataNote="Sol" onClick={(e) => playNote(e.target.dataset.note)}>
+      G
+    </Key>
+    <SharpKey dataNote="Sol#" onClick={(e) => playNote(e.target.dataset.note)}>
+      Y
+    </SharpKey>
+    <Key dataNote="La" onClick={(e) => playNote(e.target.dataset.note)}>
+      H
+    </Key>
+    <SharpKey dataNote="La#" onClick={(e) => playNote(e.target.dataset.note)}>
+      U
+    </SharpKey>
+    <Key dataNote="Si" onClick={(e) => playNote(e.target.dataset.note)}>
+      J
+    </Key>
   </PianoStyles>
 );
 
