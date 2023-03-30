567502c9b21130867ac2bcb532417db0f42e416a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-30 17:31:10 -0300

    optimizing design

--- src/components/Piano/Piano.styles.js
+++ src/components/Piano/Piano.styles.js
@@ -2,16 +2,32 @@ import styled from "styled-components";
 
 const PianoStyles = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 8px;
   width: 80vw;
-  height: calc(80vw / 3.4);
+  height: calc(88vw / 3);
   background-color: var(--color-fist);
-  padding: 2px 30px 30px;
+  padding: 8px 30px 30px;
   border-radius: 15px;
   box-shadow: 35px 35px 70px #0e1421, -35px -35px 70px #141c2d;
 
-  & button[data-note*="#"] {
+  & .keys-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+
+  & .keys-container button:first-child {
+    border-radius: 8px 0 8px 8px;
+  }
+
+  & .keys-container button:last-child {
+    border-radius: 0 8px 8px 8px;
+  }
+
+  & .keys-container button[data-note*="#"] {
     z-index: 9;
-    margin: 0 calc((-77vw / 7) - (-40vw / 5));
+    margin: 0 calc((-76.3vw / 7) - (-40vw / 5));
   }
 `;
 
